7f9b628e43864a1d5f6e3980d5edab0592b063a4
joaovictorbelo <joao.victorbelo@ufpe.br>
2020-12-01 18:37:34 -0300

    Feat(SobreNosCards): cards finalizados faltando estilização

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
 import {
   PostList,
-  CardSobre
 } from './components';
 
 function App() {
   return (
-    <CardSobre
-      titulo = "VISÃO"
-      texto = "Tornar-se uma Empresa Júnior de referência a nível local oferecendo um serviço inovador, qualificado e eficiente."
-      imagem = "https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/eye-24-512.png"
-      largura = '300'
-      altura = '350'
-    />
+    <div>
+      <PostList />
+    </div>
   );
 }
 
--- client/src/components/CardSobre/CardSobre.jsx
+++ client/src/components/CardSobre/CardSobre.jsx
@@ -8,11 +8,21 @@ function CardSobre(props) {
     const {largura} = props;
     const {altura} = props;
 
+    const textRender = (texto) => {
+        if(!Array.isArray(texto)){
+            return (
+                <p>{texto}</p>
+            );
+        } else {
+            return texto.map(linha => <span>{linha}</span>);
+        }
+    } 
+
     return (
         <div className="card" style ={{width: largura + 'px', height: altura + 'px'}}>
             <img src={imagem} alt="icone"/>
             <h2>{titulo}</h2>
-            <p>{texto}</p>  
+            <div id="texto">{textRender(texto)}</div>  
         </div>
     );
 }
