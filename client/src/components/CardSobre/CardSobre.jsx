9e24c532214de0123bcab7fe5576242f1052f27b
joaovictorbelo <joao.victorbelo@ufpe.br>
2020-12-02 15:58:53 -0300

    Feat(SobreNosCards): melhorias no design e ajustes finais

--- client/src/components/CardSobre/CardSobre.jsx
+++ client/src/components/CardSobre/CardSobre.jsx
@@ -6,17 +6,23 @@ function CardSobre(props) {
         titulo,
         texto,
         imagem,
-        largura = 300,
-        altura = 300
+        largura = 360,
+        altura = 370
     } = props;
+
+    const renderTexto = (texto) => {
+        if (Array.isArray(texto)){
+            return (texto.map(linha => <span>{linha}</span>));
+        } else {
+            return (<p>{texto}</p>)
+        }
+    }
 
     return (
         <div className="card" style ={{width: largura + 'px', height: altura + 'px'}}>
             <img src={imagem} alt="icone"/>
             <h2>{titulo}</h2>
-            <div id="texto">
-                {(Array.isArray(texto))?(texto.map(linha => <span>{linha}</span>)):(<p>{texto}</p>)}
-            </div>  
+            <div id="texto">{renderTexto(texto)}</div>  
         </div>
     );
 }
