c5ddc15aefc51294df47e356c62e4213599aed81
= <joao.victorbelo@ufpe.br>
2020-12-03 00:36:21 -0300

    feat(footerComponent): footer criado e estilizadp

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer() {
+    return(
+        <div className="footer">
+            <span>&copy; 2020 | SigaGeo Jr</span>
+            <div>
+                <span>Made with &#60;&#47;&#62; and &hearts; by </span>
+                <img src="https://res.cloudinary.com/dupufkeck/image/upload/v1606959189/output-onlinepngtools_rzqfhs.png" alt="logo citi"/>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,1 +1,2 @@
 export { default as PostList } from './PostList';
+export { default as Footer } from './Footer';
