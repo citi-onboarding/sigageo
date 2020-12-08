79e39cfd2937568064119eabfcc2c68ca9ff9f83
= <joao.victorbelo@ufpe.br>
2020-12-08 11:11:45 -0300

    feat(SlickCards): card depoimento concluido

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,5 +1,6 @@
 export { default as PostList } from './PostList';
 export { default as CardServico } from './CardServico';
 export { default as SlickServico } from './SlickServico';
+export { default as SlickDepoimentos } from './SlickDepoimentos';
 export { default as CardSobre } from './CardSobre';
 export { default as CardDepoimento } from './CardDepoimento';
