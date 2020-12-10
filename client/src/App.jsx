45430f26c19839365e582e31f19e8b2777e4b808
= <joao.victorbelo@ufpe.br>
2020-12-10 01:34:36 -0300

    feat(SobreNosCards): ajustes realizados

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
 import {
-  CardDepoimento,
+  PostList,
 } from './components';
 
 function App() {
   return (
-    <CardDepoimento
-      imagem = 'https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg'
-      nome = 'Cristiana Duarte'
-      cargo = 'Tutora Siga'
-      texto = 'A empresa Júnior SigaGeo surgiu para implantar o espírito empreendedor nos alunos de bacharelado e licenciatura em Geografia da UFPE, além de proporcionar experiências e treinamentos, em busca de desenvolvimento pessoal e profissional com inovação nas áreas de ensino de geografia, meio ambiente e geotecnologias. Essas experiências visam ainda facilitar o ingresso desses alunos no mercado de trabalho. Como professora do curso de Geografia e tutora da empresa, sinto muito orgulho dos alunos que fazem parte da Siga e do trabalho desenvolvido por eles.'
-    />
+    <PostList />
   );
 }
 
