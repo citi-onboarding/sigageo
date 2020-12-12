a7f18c58832d49bc5e3e67188cea3909a72d8412
= <joao.victorbelo@ufpe.br>
2020-12-11 23:55:31 -0300

    deploy criado

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const keystone = require('keystone');
 const cors = require('cors');
 
 const depoimentos_controller = require('../controllers/depoimentosController');
